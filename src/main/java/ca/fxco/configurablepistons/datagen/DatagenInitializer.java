a3903b1e420164749891e89acf92bd2fa315d921
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-05 23:05:10 -0400

    Automate last tags required

--- src/main/java/ca/fxco/configurablepistons/datagen/DatagenInitializer.java
+++ src/main/java/ca/fxco/configurablepistons/datagen/DatagenInitializer.java
@@ -12,12 +12,6 @@ public class DatagenInitializer implements DataGeneratorEntrypoint {
 
     public static List<Block> datagenBlockList = new ArrayList<>();
 
-    // TODO: Add every single block tag to the datagen, don't do anything manually
-    // Missing:
-    //  - configurable-pistons.tags.block.moving_pistons
-    //  - configurable-pistons.tags.block.pistons
-    //  - configurable-pistons.tags.block.sticky_blocks
-    //  - configurable-pistons.tags.block.unpushable
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         ModBlocks.BASIC_PISTON.getName(); // Crucial, makes sure that mod blocks are loaded before doing datagen
--- src/main/java/ca/fxco/configurablepistons/datagen/PistonBlockTagProvider.java
+++ src/main/java/ca/fxco/configurablepistons/datagen/PistonBlockTagProvider.java
@@ -1,13 +1,16 @@
 package ca.fxco.configurablepistons.datagen;
 
+import ca.fxco.configurablepistons.base.ModBlocks;
+import ca.fxco.configurablepistons.base.ModTags;
+import ca.fxco.configurablepistons.blocks.pistons.basePiston.BasicPistonBlock;
 import ca.fxco.configurablepistons.blocks.pistons.basePiston.BasicPistonExtensionBlock;
 import ca.fxco.configurablepistons.pistonLogic.families.PistonFamilies;
 import ca.fxco.configurablepistons.pistonLogic.families.PistonFamily;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.tag.BlockTags;
-import net.minecraft.tag.TagKey;
 
 public class PistonBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
@@ -17,20 +20,29 @@ public class PistonBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
     @Override
     protected void generateTags() {
-        FabricTagBuilder<Block> dragonImmuneBuilder = tag(BlockTags.DRAGON_IMMUNE);
-        FabricTagBuilder<Block> witherImmuneBuilder = tag(BlockTags.WITHER_IMMUNE);
+        FabricTagBuilder<Block> dragonImmuneBuilder = getOrCreateTagBuilder(BlockTags.DRAGON_IMMUNE);
+        FabricTagBuilder<Block> witherImmuneBuilder = getOrCreateTagBuilder(BlockTags.WITHER_IMMUNE);
+        FabricTagBuilder<Block> movingPistonBuilder = getOrCreateTagBuilder(ModTags.MOVING_PISTONS);
+        FabricTagBuilder<Block> stickyBlocksBuilder = getOrCreateTagBuilder(ModTags.STICKY_BLOCKS);
+        FabricTagBuilder<Block> unpushableBuilder = getOrCreateTagBuilder(ModTags.UNPUSHABLE);
+        FabricTagBuilder<Block> pistonsBuilder = getOrCreateTagBuilder(ModTags.PISTONS);
         System.out.println("Amount of Piston Families: "+PistonFamilies.getFamilies().size());
         for (PistonFamily family : PistonFamilies.getFamilies()) {
             if (!family.shouldGenerateAutomatically()) continue;
-            BasicPistonExtensionBlock basicPistonExtensionBlock = family.getExtensionBlock();
-            if (basicPistonExtensionBlock != null) {
-                dragonImmuneBuilder.add(basicPistonExtensionBlock);
-                witherImmuneBuilder.add(basicPistonExtensionBlock);
-            }
+            BasicPistonBlock pistonBlock = family.getPistonBlock(); // Pistons - pistonBlock
+            if (pistonBlock != null) pistonsBuilder.add(pistonBlock);
+            BasicPistonBlock stickyPistonBlock = family.getStickyPistonBlock(); // Pistons - stickyPistonBlock
+            if (stickyPistonBlock != null) pistonsBuilder.add(stickyPistonBlock);
+            BasicPistonExtensionBlock movingPistonBlock = family.getExtensionBlock(); // Moving_Pistons
+            if (movingPistonBlock != null) movingPistonBuilder.add(movingPistonBlock);
         }
-    }
-
-    public FabricTagBuilder<Block> tag(TagKey<Block> tag) {
-        return getOrCreateTagBuilder(tag);
+        // Populate custom tags
+        pistonsBuilder.add(Blocks.PISTON, Blocks.STICKY_PISTON);
+        movingPistonBuilder.add(Blocks.MOVING_PISTON);
+        stickyBlocksBuilder.add(Blocks.SLIME_BLOCK, Blocks.HONEY_BLOCK, ModBlocks.SLIMY_REDSTONE_BLOCK);
+        unpushableBuilder.add(Blocks.OBSIDIAN, Blocks.CRYING_OBSIDIAN, Blocks.RESPAWN_ANCHOR);
+        // Add tags to vanilla tags
+        dragonImmuneBuilder.addTag(ModTags.MOVING_PISTONS);
+        witherImmuneBuilder.addTag(ModTags.MOVING_PISTONS);
     }
 }
