40fb295386918eacd29127a453337d5e31c14c1b
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-01-15 12:44:01 -0500

    `onRemove` was removed cause it was not needed when the block gets removed

--- src/main/java/ca/fxco/pistonlib/blocks/PoweredStickyBlock.java
+++ src/main/java/ca/fxco/pistonlib/blocks/PoweredStickyBlock.java
@@ -46,13 +46,6 @@ public class PoweredStickyBlock extends DirectionalBlock implements Configurable
     }
 
     @Override
-    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
-        if (!level.isClientSide() && !newState.is(this) && !movedByPiston && state.getValue(POWERED)) {
-            updatePowered(state, level, pos, true);
-        }
-    }
-
-    @Override
     public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
         if (!level.isClientSide() && !oldState.is(this) && state.getValue(POWERED)) {
             updatePowered(state, level, pos, true);
