f597053fe52eec18a158d18e3ee4ad8501a942f3
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-01-18 15:53:20 -0500

    Add all items to the general creative tab Split pistons into their own tab

--- src/main/java/ca/fxco/pistonlib/base/ModCreativeModeTabs.java
+++ src/main/java/ca/fxco/pistonlib/base/ModCreativeModeTabs.java
@@ -1,18 +1,55 @@
 package ca.fxco.pistonlib.base;
 
+import ca.fxco.pistonlib.pistonLogic.families.PistonFamily;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 
 import static ca.fxco.pistonlib.PistonLib.id;
 
 public class ModCreativeModeTabs {
 
     public static final CreativeModeTab GENERAL = FabricItemGroup.builder(id("general"))
+        .icon(() -> new ItemStack(ModItems.BASIC_STICKY_PISTON))
+        .displayItems((featureFlags, output, hasPermissions) -> {
+            output.accept(ModItems.HALF_SLIME_BLOCK);
+            output.accept(ModItems.HALF_HONEY_BLOCK);
+            output.accept(ModItems.HALF_REDSTONE_BLOCK);
+            output.accept(ModItems.HALF_OBSIDIAN_BLOCK);
+            output.accept(ModItems.HALF_REDSTONE_LAMP_BLOCK);
+
+            output.accept(ModItems.DRAG_BLOCK);
+            output.accept(ModItems.STICKYLESS_BLOCK);
+            output.accept(ModItems.STICKY_TOP_BLOCK);
+            output.accept(ModItems.SLIMY_REDSTONE_BLOCK);
+            output.accept(ModItems.ALL_SIDED_OBSERVER);
+            output.accept(ModItems.GLUE_BLOCK);
+            output.accept(ModItems.POWERED_STICKY_BLOCK);
+            output.accept(ModItems.STICKY_CHAIN_BLOCK);
+            output.accept(ModItems.AXIS_LOCKED_BLOCK);
+            output.accept(ModItems.MOVE_COUNTING_BLOCK);
+            output.accept(ModItems.WEAK_REDSTONE_BLOCK);
+            output.accept(ModItems.QUASI_BLOCK);
+
+            output.accept(ModItems.SLIPPERY_SLIME_BLOCK);
+            output.accept(ModItems.SLIPPERY_REDSTONE_BLOCK);
+            output.accept(ModItems.SLIPPERY_STONE_BLOCK);
+
+            output.accept(ModItems.OBSIDIAN_SLAB_BLOCK);
+            output.accept(ModItems.OBSIDIAN_STAIR_BLOCK);
+        })
+        .build();
+
+    public static final CreativeModeTab PISTONS = FabricItemGroup.builder(id("pistons"))
         .icon(() -> new ItemStack(ModItems.STRONG_STICKY_PISTON))
         .displayItems((featureFlags, output, hasPermissions) -> {
-            // TODO
+            for (PistonFamily family : ModRegistries.PISTON_FAMILY) {
+                for (Block base : family.getBases().values()) {
+                    output.accept(base);
+                }
+            }
         })
         .build();
 
--- src/main/java/ca/fxco/pistonlib/base/ModItems.java
+++ src/main/java/ca/fxco/pistonlib/base/ModItems.java
@@ -1,6 +1,5 @@
 package ca.fxco.pistonlib.base;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -75,7 +74,6 @@ public class ModItems {
     }
 
     private static <T extends Item> T register(ResourceLocation id, T item) {
-        ItemGroupEvents.modifyEntriesEvent(ModCreativeModeTabs.GENERAL).register(content -> content.accept(item));
         return Registry.register(BuiltInRegistries.ITEM, id, item);
     }
 
